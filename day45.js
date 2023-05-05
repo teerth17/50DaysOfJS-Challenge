16eac173f293036a5b4cb09f1437b23480fcdcfa
Teerth Patel <76660982+teerth17@users.noreply.github.com>
2023-05-05 14:02:11 +0530

    Create day45.js

---
+++ day45.js
@@ -0,0 +1,9 @@
+/*
+A Needle in the Haystack
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+*/
+
+function findNeedle(haystack) {
+  // your code here
+  return `found the needle at position ${haystack.indexOf("needle")}`;
+}
